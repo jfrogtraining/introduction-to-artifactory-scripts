dcceffe4ce9e36f27bda560a01708528694ddaa7
jroquelaure <jonathanr@jfrog.com>
2016-05-22 23:08:23 +0200

    add clone command for project examples

--- license.sh
+++ license.sh
@@ -1,3 +1,6 @@
+#For linux user : uncomment one of the commands above according to your distribution ( and comment the first curl command whic is for mac users)
+#curl -o jfrog -H "X-JFrog-Art-Api:AKCp2UNgwajwpoz5PghBirpgR5xafggbFyCkSU4DTqK2fQPd9thVcar7wHqk2BNmYjg1km6vJ" http://192.168.1.139:8081/artifactory/jswampup-remote-cache/cli-1.2/linux-32/jfrog
+#curl -o jfrog -H "X-JFrog-Art-Api:AKCp2UNgwajwpoz5PghBirpgR5xafggbFyCkSU4DTqK2fQPd9thVcar7wHqk2BNmYjg1km6vJ" http://192.168.1.139:8081/artifactory/jswampup-remote-cache/cli-1.2/linux-64/jfrog
 curl -o jfrog -H "X-JFrog-Art-Api:AKCp2UNgwajwpoz5PghBirpgR5xafggbFyCkSU4DTqK2fQPd9thVcar7wHqk2BNmYjg1km6vJ" http://192.168.1.139:8081/artifactory/jswampup-remote-cache/cli-1.2/mac/jfrog
 chmod 755 jfrog
 ./jfrog mc c --url=http://192.168.1.139:8080 --user=admin --password=password --interactive=false 
